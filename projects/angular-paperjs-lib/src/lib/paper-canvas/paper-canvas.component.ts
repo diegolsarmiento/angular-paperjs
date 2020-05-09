3d7b6b4d354214c5a44fbd67583e7caec5cc4947
Diego Sarmiento <dlsarmiento@gmail.com>
2020-05-08 20:20:39 -0400

    Fixed lifecyle + added route + basic config

--- projects/angular-paperjs-lib/src/lib/paper-canvas/paper-canvas.component.ts
+++ projects/angular-paperjs-lib/src/lib/paper-canvas/paper-canvas.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { PaperScope, Project, Path, Point } from 'paper';
+import { Component, AfterViewInit, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { PaperScope, Project, Path, Layer } from 'paper';
 import { canvasSize } from '../settings';
 
 @Component({
@@ -7,21 +7,56 @@ import { canvasSize } from '../settings';
   templateUrl: './paper-canvas.component.html',
   styleUrls: ['./paper-canvas.component.scss']
 })
-export class PaperCanvasComponent implements OnInit {
+export class PaperCanvasComponent implements OnInit, AfterViewInit {
 
   @ViewChild('canvasElement') canvasElement: ElementRef;
+   /* Basic config */
   height: string;
   width: string;
   scope: any;
   project: any;
 
+  /* addElements */
+  currentPath: any;
+  path: any;
+  secondLayer: any;
+  secondPath: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    /* Basic config */
     this.height = canvasSize.height;
     this.width = canvasSize.width;
     this.scope = new PaperScope();
+  }
+
+  ngAfterViewInit(): void {
     this.project = new Project(this.canvasElement.nativeElement);
+    this.addElements();
+  }
+
+  addElements() {
+    // Create a circle shaped path
+    this.currentPath = new Path();
+    this.path = new Path.Circle({
+      center: [80, 50],
+      radius: 35,
+      fillColor: 'red'
+    });
+
+    // Create a new layer and activate it
+    this.secondLayer = new Layer();
+
+    // Second path is added as a child of the second layer
+    this.secondPath = new Path.Circle({
+      center: [120, 50],
+      radius: 35,
+      fillColor: '#00FF00'
+    });
+
+    // Add child to layer
+    this.project.activeLayer.addChild(this.currentPath);
   }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../app/home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**',   redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
