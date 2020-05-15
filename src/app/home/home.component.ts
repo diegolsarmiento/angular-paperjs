d79bb426b2c52f4b2737dc5904fca4180d2e27e6
Diego Sarmiento <dlsarmiento@gmail.com>
2020-05-15 19:06:49 -0400

    Added fontexperiment to top layer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  year: number;
+  fontFamly: string;
+  fontSize: string;
+  fontSizeNumber: number;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.randomFont();
+    this.ramdomSizes();
+  }
+
+  ramdomSizes() {
+    this.year = this.randomNumber(1960, 2828);
+    this.fontSizeNumber = this.randomNumber(92, 98);
+    this.fontSize = this.fontSizeNumber + 'px';
+  }
+
+  randomFont(){
+    const fontsArray = ['Righteous', 'Fjalla One', 'Teko'];
+    const fontsArrayLength = fontsArray.length;
+    this.fontFamly = fontsArray[this.randomNumber(0, fontsArrayLength)];
+
+  }
+
+  randomNumber(min, max): number {
+    const random = Math.floor(Math.random() * (max - min) + min);
+    return random;
   }
 
 }
